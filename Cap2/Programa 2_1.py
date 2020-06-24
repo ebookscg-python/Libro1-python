e0dd6e6186958eac7f24b729a0dc40a893db1617
ebookscg-python <67077392+ebookscg-python@users.noreply.github.com>
2020-06-24 09:23:08 -0500

    Update Programa 2_1.py

--- Cap2/Programa 2_1.py
+++ Cap2/Programa 2_1.py
@@ -5,7 +5,7 @@ Problema 2.1
 Calcula e imprime el promedio de dos números enteros.
 """
 
-entero1 = int(input('Ingresa un número entero: '))
-entero2 = int(input('Ingresa un número entero: '))
+entero1 = int(input('Ingrese un número entero: '))
+entero2 = int(input('Ingrese un número entero: '))
 promedio = (entero1 + entero2) / 2
 print('\nPromedio:', promedio)
