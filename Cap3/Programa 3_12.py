a5cb89059f89ecbef07e0132161721809aa455ed
ebookscg-python <67077392+ebookscg-python@users.noreply.github.com>
2020-06-24 16:36:26 -0500

    Update Programa 3_12.py

--- Cap3/Programa 3_12.py
+++ Cap3/Programa 3_12.py
@@ -6,7 +6,7 @@ Dado un número entero, determina e imprime un mensaje indicando
 si el número es par o impar.
 """
 
-entero = int(input('Ingresa un número entero: '))
+entero = int(input('Ingrese un número entero: '))
 if entero % 2 == 0:
     print('El número es par.'.center(40))
 else:
