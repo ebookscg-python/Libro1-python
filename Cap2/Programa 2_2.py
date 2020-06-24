efe3a530d187e3e3c6d724a78d7261e092db4bd1
ebookscg-python <67077392+ebookscg-python@users.noreply.github.com>
2020-06-24 09:28:45 -0500

    Update Programa 2_2.py

--- Cap2/Programa 2_2.py
+++ Cap2/Programa 2_2.py
@@ -5,8 +5,8 @@ Problema 2.2
 Imprime en orden inverso los 4 números leídos.
 """
 
-entero1 = int(input('Ingresa un número entero: '))
-entero2 = int(input('Ingresa un número entero: '))
-entero3 = int(input('Ingresa un número entero: '))
-entero4 = int(input('Ingresa un número entero: '))
+entero1 = int(input('Ingrese un número entero: '))
+entero2 = int(input('Ingrese un número entero: '))
+entero3 = int(input('Ingrese un número entero: '))
+entero4 = int(input('Ingrese un número entero: '))
 print("\n", entero4, '\t', entero3, '\t', entero2, '\t', entero1)
